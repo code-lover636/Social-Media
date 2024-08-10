c8c2234a9f83ca3ded7db410d18401bfe83573be
alexabraham029 <152091409+alexabraham029@users.noreply.github.com>
2024-08-10 20:24:45 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,8 +6,27 @@ load_dotenv()
 
 PASSWORD = os.getenv("MYSQL_DB_PASSWORD")
 
-def register_user(user_name, user_email, password):
-    pass
+def register_user(first_name, second_name, email_id, password, dob):
+    connection = create_connection()
+    cursor = connection.cursor()
+    
+    
+    cursor.execute("SELECT * FROM USERS WHERE EMAIL_ID = %s", (email_id,))
+    if cursor.fetchone():
+        print("User already exists!")
+        cursor.close()
+        connection.close()
+        return False
+    
+    
+    sql = "INSERT INTO USERS (FIRST_NAME, SECOND_NAME, EMAIL_ID, PASSWORD, DOB) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, (first_name, second_name, email_id, password, dob))
+    connection.commit()
+    
+    print("User registered successfully!")
+    cursor.close()
+    connection.close()
+    return True
 
 def login_user(user_email, password):
     pass
